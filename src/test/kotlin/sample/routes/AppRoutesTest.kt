519c962d4ccf1698f1ad788354e60512b54fbecf
Biju Kunjummen <biju.kunjummen@gmail.com>
2017-08-31 18:07:06 -0700

    More samples

--- src/test/kotlin/sample/routes/AppRoutesTest.kt
+++ src/test/kotlin/sample/routes/AppRoutesTest.kt
@@ -1,9 +1,16 @@
 package sample.routes
 
 import org.junit.Test
+import org.springframework.core.io.ClassPathResource
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.web.reactive.function.BodyInserters
+import org.springframework.web.reactive.function.bodyFromPublisher
 import org.springframework.web.reactive.function.server.ServerResponse.ok
+import org.springframework.web.reactive.function.server.ServerResponse.status
 import org.springframework.web.reactive.function.server.router
+import sample.domain.Message
 
 class AppRoutesTest {
 
@@ -33,53 +40,53 @@ class AppRoutesTest {
                 .expectStatus().isOk
     }
 
-//    @Test
-//    fun testNested() {
-//        val routerFunction = router {
-//            ("/api" and accept(MediaType.APPLICATION_JSON) and contentType(MediaType.APPLICATION_JSON)).nest {
-//                POST("/create", { req ->
-//                    status(HttpStatus.CREATED).body(bodyFromPublisher(req.bodyToMono(Message::class.java)))
-//                })
-//                PUT("/update", { req ->
-//                    status(HttpStatus.ACCEPTED).body(bodyFromPublisher(req.bodyToMono(Message::class.java)))
-//                })
-//            }
-//        }
-//
-//        val client = WebTestClient.bindToRouterFunction(routerFunction).build()
-//
-//        client.post()
-//                .uri("/api/create")
-//                .body(BodyInserters.fromObject(Message("1", "one")))
-//                .exchange()
-//                .expectStatus().isCreated
-//                .expectBody()
-//                .jsonPath("$.payload")
-//                .isEqualTo("one")
-//
-//        client.put()
-//                .uri("/api/update")
-//                .body(BodyInserters.fromObject(Message("1", "one")))
-//                .exchange()
-//                .expectStatus().isAccepted
-//                .expectBody()
-//                .jsonPath("$.payload")
-//                .isEqualTo("one")
-//    }
-//
-//    @Test
-//    fun testResources() {
-//        val routerFunction = router {
-//            resources("/someresource/**",
-//                    ClassPathResource("/someresource/response.txt"))
-//        }
-//        
-//        val client = WebTestClient.bindToRouterFunction(routerFunction).build()
-//
-//        client.get()
-//                .uri("/someresource/response.txt")
-//                .exchange()
-//                .expectStatus().isOk
-//
-//    }
+    @Test
+    fun testNested() {
+        val routerFunction = router {
+            ("/api" and accept(MediaType.APPLICATION_JSON) and contentType(MediaType.APPLICATION_JSON)).nest {
+                POST("/create", { req ->
+                    status(HttpStatus.CREATED).body(bodyFromPublisher(req.bodyToMono(Message::class.java)))
+                })
+                PUT("/update", { req ->
+                    status(HttpStatus.ACCEPTED).body(bodyFromPublisher(req.bodyToMono(Message::class.java)))
+                })
+            }
+        }
+
+        val client = WebTestClient.bindToRouterFunction(routerFunction).build()
+
+        client.post()
+                .uri("/api/create")
+                .body(BodyInserters.fromObject(Message("1", "one")))
+                .exchange()
+                .expectStatus().isCreated
+                .expectBody()
+                .jsonPath("$.payload")
+                .isEqualTo("one")
+
+        client.put()
+                .uri("/api/update")
+                .body(BodyInserters.fromObject(Message("1", "one")))
+                .exchange()
+                .expectStatus().isAccepted
+                .expectBody()
+                .jsonPath("$.payload")
+                .isEqualTo("one")
+    }
+
+    @Test
+    fun testResources() {
+        val routerFunction = router {
+            resources("/someresource/**",
+                    ClassPathResource("/someresource/response.txt"))
+        }
+
+        val client = WebTestClient.bindToRouterFunction(routerFunction).build()
+
+        client.get()
+                .uri("/someresource/response.txt")
+                .exchange()
+                .expectStatus().isOk
+
+    }
 }
